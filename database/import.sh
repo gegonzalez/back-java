d8bb0bbfdd1f19f85eed8e2aefb51df7dbc34260
german.gonzalez <german.gonzalez@rappi.com>
2020-12-21 20:34:10 -0300

    updating docker-compose

--- database/import.sh
+++ database/import.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+. ./env
 HOST=$1
 PROJECT_DIRECTORY=$2
 FILES=$(ls ${PROJECT_DIRECTORY}/database/*.json | sort -n -t _ -k 2)
 
 while : ; do
-    CHECK=$(mongo --host ${HOST} --username brandDiscountsUser --password brandDiscountsPassword --authenticationDatabase admin promotions --eval 'db.version() 2> /dev/null')
+    CHECK=$(mongo --host ${DB_HOST} --username ${DB_USERNAME} --password ${DB_PASSWORD} --authenticationDatabase admin promotions --eval 'db.version() 2> /dev/null')
     if [[ $CHECK == *"MongoDB server version"* ]]
     then
         break
@@ -19,7 +20,7 @@ for AFILE in ${FILES[@]}
 do
     echo -e "[$(date)] Processing \t$AFILE"
     COLLECTION=`echo $AFILE | sed -n 's/.*\-\(.*\).json/\1/p'`
-    mongoimport --host ${HOST} --username brandDiscountsUser --password brandDiscountsPassword --authenticationDatabase admin --db desafio_walmart --collection ${COLLECTION} --mode upsert --file ${AFILE}
+    mongoimport --host ${DB_HOST} --username ${DB_USERNAME} --password ${DB_PASSWORD} --authenticationDatabase admin --db ${DB_DATABASE} --collection ${COLLECTION} --mode upsert --file ${AFILE}
     echo -e "[$(date)] Done \t\t$AFILE"
 done
 
